1edc2e96cdff7190742fd2251ccf365974f38939
Siddharth Bhaiji <siddharth10297@gmail.com>
2023-12-22 19:42:33 +0530

    fix router with context

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,23 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import NotFound from '../pages/NotFound';
 import UserProfile from '../pages/UserProfile';
 import UsersDirectory from '../pages/UsersDirectory';
+import UsersContextProvider from '../contexts/Users';
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="" element={<Navigate to="/users" />} />
-      <Route path="/users" element={<UsersDirectory />} />
-      <Route path="/users/:id" element={<UserProfile />} />
+      <Route
+        path="/users/*"
+        element={
+          <UsersContextProvider>
+            <Routes>
+              <Route index element={<UsersDirectory />} />
+              <Route path=":id" element={<UserProfile />} />
+            </Routes>
+          </UsersContextProvider>
+        }
+      />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
