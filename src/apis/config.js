09ae51311abcce46d6dd3332230bf7d93cbc8f24
Siddharth Bhaiji <siddharth10297@gmail.com>
2024-01-07 11:29:50 +0530

    add todo note

--- src/apis/config.js
+++ src/apis/config.js
@@ -1,9 +1,11 @@
-import axios from 'axios';
+import axios from 'axios'
+
+// TODO: Create instances for diff api services
 
 const instance = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-});
+})
 
-export default instance;
+export default instance
